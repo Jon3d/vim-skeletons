923470dfa77369d4f4ae16996d5b9dfb1af0ad40
Jonathan Huisingh <jon.huisingh@gmail.com>
2014-05-28 19:14:18 -0700

    initial commit

---
+++ skeletons/skel.java
@@ -0,0 +1,12 @@
+// TODO package name;
+ 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+ 
+public class @FILE@ {
+	public static void main(String[] args) {
+ 
+
+	}
+}
